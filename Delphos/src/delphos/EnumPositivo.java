761a10a75ae311b021265e85bdf33f52f11318bc
Anne Caroline Silva <carolinesilva4@hotmail.com>
2019-04-17 20:57:57 -0300

    Inserção dos enum

---
+++ Delphos/src/delphos/EnumPositivo.java
@@ -0,0 +1,24 @@
+/*
+ * Anne Caroline Silva
+ * 16/04/2019
+ */
+
+package delphos;
+
+public enum EnumPositivo {
+	VERYINACCURATE(1),
+	MODERATELYINACCURATE(2),
+	NEUTRAL(3),
+	MODERATELYACCURATE(4),
+	VERYACCURATE(5);
+	
+	private int resp;
+	
+	EnumPositivo(int resp){
+		this.resp = resp;
+	}
+	
+	public int getResult(){
+		return this.resp;
+	}
+}
