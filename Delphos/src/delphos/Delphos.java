a5b4f329d4f6770137847b4830c40934a12127f3
Anne Caroline Silva <carolinesilva4@hotmail.com>
2019-05-17 20:54:38 -0300

    Novos testes na main

--- Delphos/src/delphos/Delphos.java
+++ Delphos/src/delphos/Delphos.java
@@ -24,14 +24,26 @@ public class Delphos {
 		/* cria um dicionario com chave:ID_Pergunta e valor:DescricaoPergunta */
 		HashMap<String, String> dicP = todasPerguntas.ListaPerguntas();
 		
-		/* teste que imprime todas as perguntas com seus respectivos IDs */
+		/* teste que imprime todas as perguntas com seus respectivos IDs
 		System.out.println("Teste que imprime IDs e Descrição da pergunta");
 		for(Map.Entry<String, String> entry : dicP.entrySet()) {
 			String key = entry.getKey();
 		    String value = entry.getValue();
 			System.out.println(key + "  " + value);
-		}
+		}*/
     	
+		/* Opção do Menu: Realiza teste */
+		RealizaTeste novoTeste = new RealizaTeste();
+		HashMap<String, Integer> dicR = novoTeste.realizaTeste(dicP);
+		
+		
+		/* teste que imprime todas as respostas com seus respectivos IDs 
+		for(Map.Entry<String, Integer> entry : dicR.entrySet()) {
+			String key = entry.getKey();
+		    Integer value = entry.getValue();
+			System.out.println(key + "  " + value);
+		}*/
+		
     }
     
 }
